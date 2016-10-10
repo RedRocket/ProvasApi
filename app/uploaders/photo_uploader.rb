4398de0066ba5a94fb25c2f667d1cdfd7c6ff57f
Guilherme Carneiro <gui.carneiro15@gmail.com>
2016-10-10 11:42:05 -0300

    thumb

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 
 class PhotoUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_fit: [800, 800]
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -9,6 +11,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :fog
   # storage :fog
+
+  version :thumb do
+    process resize_to_fill: [200,200]
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
