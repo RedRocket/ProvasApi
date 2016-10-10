fc65eaf7588bccb7d76ad8ec3415eba272be15e3
Guilherme Carneiro <gui.carneiro15@gmail.com>
2016-10-10 15:23:03 -0300

    update thumb

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -7,16 +7,15 @@ class Api::FavoritesController < ApplicationController
   end
 
   def destroy
-    @favorite = Favorite.where(exam_id: params["exam_id"], user_id: params["user_id"])
+    @favorite = Favorite.find(id: params["id"])
 
-    if(@favorite != nil)
-      @favorite.destroy
-    end
+    @favorite.destroy
+
     render json: {}.to_json(), status: :ok
   end
 
   def index
-    @favorites = User.find_by(id: params["id"]).favorites.pluck(:exam_id)
+    @favorites = User.find(params["id"]).favorites
 
     render json: @favorites.to_json(), status: :ok
   end
--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,7 @@ Rails.application.routes.draw do
 
     get "favorites/:id" => 'favorites#index'
     post "favorites/" => 'favorites#create'
-    post "favorites/delete" => 'favorites#destroy'
+    delete "favorites/:id" => 'favorites#destroy'
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
