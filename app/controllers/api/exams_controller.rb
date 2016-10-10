6fde2909aea98f86db6a0c8f3b1fa7030646e800
Guilherme Carneiro <gui.carneiro15@gmail.com>
2016-10-10 17:22:01 -0300

    update thumb

--- app/controllers/api/exams_controller.rb
+++ app/controllers/api/exams_controller.rb
@@ -73,6 +73,6 @@ class Api::ExamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def exam_params
-      params.permit(:name, :subject_id, :views, :period, :user_id, :exam_id, :image)
+      params.permit(:name, :subject_id, :views, :period, :user_id, :exam_id, :image, :anonymous)
     end
 end
