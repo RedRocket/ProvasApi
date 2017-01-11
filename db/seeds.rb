ad4d34e9aee96026fa3955afaf3c94f0579714e8
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-01-11 12:30:11 -0300

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,21 +38,7 @@ states = [
 ]
 
 cities = [
-  ['Rio Branco', 'Cruzeiro do Sul'],
-  ['Maceió', 'Arapiraca', 'Palmeira dos Índios', 'Pão de Açucar', 'Panedo'],
-  ['Manaus'],
-  ['Macapá', 'Laranjal do Jari', 'Santana'],
-  ['Salvador', 'Alagoinhas', 'Barreiras', 'Camaçari', 'Capim Grosso', 'Campo Formoso', 'Cruz das Almas', 'Eunápolis', 'Feira de Santana', 'Guanambi', 'Ibicarí', 'Ilhéus', 'Ipiaú', 'Ipirá', 'Itabuna', 'Itamaraju', 'Itapetinga', 'Jequié', 'Juazeiro', 'Lauro de Freitas', 'Luís Edu. Magalhães', 'Paulo Afonso', 'Paripiranga', 'Riachão do Jacuípe', 'Santa Cruz de Cabrália', 'Santo Ant. de Jesus', 'Senhor do Bonfim', 'Teixeira de Freitas', '
-    Vitória da Conquista'],
-  ['Fortaleza', 'Aracati', 'Caucaia', 'Crateús', 'Crato', 'Icó', 'Juazeiro do Norte', 'Redenção', 'Sobral', 'Quixadá'],
-  ['Brasília', 'Gama', 'Taguatinga'],
-  ['Vitória', 'Alegre', 'Aracruz', 'Barra S. Francisco', 'Cach. de Itaperim', 'Cariacica', 'Castelo', 'Colatina', 'Guarapari', 'Iúna', 'Nova Venécia', 'S. Maria de Jetibá', 'Santa Teresa', 'Serra', 'São Mateus', 'Venda N. Imigrante', 'Viana', 'Vila Velha'],
-  ['Goiânia', 'Anápolis', 'Anicuns', 'Caldas Novas', 'Catalão', 'Cristalina', 'Formosa', 'Goiás', 'Goiatuba', 'Iporá', 'Itaberaí', 'Itumbiara', 'Jataí', 'Luziânia', 'Mineiros', 'Pires do Rio', 'Quirinópolis', 'Rio Verde', 'Rubiataba', 'S.L. Montes Belos', 'Trindade', 'Uruaçu', 'Valparaíso de Goiás'],
-  ['São Luís', 'Bacabal', 'Balsas', 'Caxias', 'Chapadinha', 'Codó', 'Imperatriz', 'Paço do Lumiar', 'Pedreiras', 'Pinheiro', 'São José de Ribamar', 'Timon'],
-  ['Cuiabá', 'Água Boa', 'Alta Floresta', 'Araputanga', 'Barra do Garças', 'Cárceres', 'Campo Verde', 'Colíder', 'Diamantino', 'Guaratã do Norte', 'Itaúba', 'Jaciara', 'Juína', 'Lucas do Rio Verde', 'Nova Mutum', 'Nova Xavantina', 'Rondonópolis', 'S.J. Quatro Marcos', 'Sinop', 'Sorriso', 'Tangará da Serra', 'Várzea Grande'],
-  ['Campo Grande', 'Amambaí', 'Cassilândia', 'Chapadão do Sul', 'Corumbá', 'Costa Rica', 'Dourados', 'Nova Andradina', 'Paranaíba', 'Ponta Porã', 'Selvíria', 'Três Lagoas'],
-  ['Belo Horizonte', 'Além Paraíba', 'Alfenas', 'Almenara', 'Araxá', 'Barbacena', 'Bicas', 'Boa Esperança', 'Bom Despacho', 'Brumadinho', 'Cambuí', 'Campanha', 'Campina Verde', 'Campo Belo', 'Campos Gerais', 'Carangola', 'Carantiga', 'Cataguases', 'Cláudio', 'Congonhas', 'Contagem', 'Cons. Lafaiete', 'Conselheiro Pena', 'Coromandel', 'Coronel Fabriciano', 'Cruzeiro do Sul', 'Curvelo', 'Diamantina', 'Divinópolis', 'Dores do Indaiá', 'Extrema', 'Formiga', 'Frutal', 'Gov. Valadares', 'Guanhães', 'Guaxupé', 'Iguatama', 'Ipitanga', 'Itabira', 'Itajubá', 'Itaúna', 'Ituiutuba', 'Iturama', 'João Monlevade', 'João Pinheiro', 'Juatuba', 'Lavras', 'Juiz de Fora', 'Luz',  'Machado', 'Manhaçu', 'Mantena', 'Matipó', 'Mato Verde', 'Mariana', 'Monte Carmelo', 'Monte Claros', 'Muriaé', 'Nanuque', 'Nova Lima', 'Nova Porteirinha', 'Nova Serrana', 'Oliveira', 'Ouro Fino', 'Ouro Preto', 'Paracatu', 'Pará de Minas', 'Paraisópolis', 'Passos', 'Patos de Minas', 'Patrocínio', 'Pedro Leopoldo', 'Piranga', 'Pirapora', 'Piumhi', 'Ponte Nova', 'Pouso Alegre', 'Reduto', 'Sabará', 'Sabinópolis', 'Santa Luzia', 'S. Rita do Sapucaí', 'Santos Dumont', 'São Gotardo', 'São João Del Rei', 'São Lourenço', 'São S. do Paraíso', 'Sete Lagoas', 'Teófilo Otoni', 'Três Corações'],
-
+  ['Fortaleza'],
 ]
 
 Admin.create(
