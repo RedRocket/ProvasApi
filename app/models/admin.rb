4e0c36f1cebe223d0a7a5969a59b89b10430dfb8
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-01-11 21:00:07 -0300

    fixing admin hash

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,6 +2,6 @@ class Admin < ActiveRecord::Base
   before_save :create_hashed_password, if: :password_changed?
 
   def create_hashed_password
-    self.password = password.crypt("ChaveDoProvas")
+    self.password = self.password.crypt("ChaveDoProvas")
   end
 end
