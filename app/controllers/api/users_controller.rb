4811a74394902dfa3c8430dd1332cb672a75b53a
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-02-24 11:52:43 -0300

    testing

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -69,9 +69,8 @@ class Api::UsersController < ApplicationController
   def update_image
     @user = User.find_by token: user_params["token"]
     @user.remove_picture!
-    @user.picture = params[:picture]
 
-    if @user.save
+    if @user.update(picture: params[:picture])
       render json: @user.to_json(), status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
