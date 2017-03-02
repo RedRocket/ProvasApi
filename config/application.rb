b0598933fd431df76cb7004967595f32b7c9551f
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-03-02 19:43:28 -0300

    fixing

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,6 @@ module Truckvilleapi
     end
 
     CarrierWave.configure do |config|
-      config.fog_provider = 'fog/aws'                        # required
       config.fog_credentials = {
         provider:              'AWS',                        # required
         aws_access_key_id:     ENV["S3_ACCESS_KEY"],                        # required
