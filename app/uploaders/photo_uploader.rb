48602319a359d8cf90389cc87107d8a51d6c6da0
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-03-03 00:33:44 -0300

    testing diferent configuration

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -54,8 +54,4 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-    hex = SecureRandom.hex(32)
-    "image-#{hex}.jpg"
-  end
 end
