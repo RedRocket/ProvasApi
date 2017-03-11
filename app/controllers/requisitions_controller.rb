8e0741763769e2c365db769eac45997afc9768b9
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-03-11 16:28:54 -0300

    testing show user

--- app/controllers/requisitions_controller.rb
+++ app/controllers/requisitions_controller.rb
@@ -12,7 +12,7 @@ class RequisitionsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_requisition
-      @resquisition = Requisition.find(params[:id])
+      @requisition = Requisition.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
