926c755498562a604eb4e3ab73135f345d9e60bb
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-04-17 17:31:30 -0300

    adding complaints via api

--- app/controllers/api/complaints_controller.rb
+++ app/controllers/api/complaints_controller.rb
@@ -1,4 +1,13 @@
 class Api::ComplaintsController < ApplicationController
   def create
+    @complaint = Complaint.create(complaint_params)
+
+    render json: @exam.to_json(), status: :ok
   end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def complaint_params
+      params.require(:complaint).permit(:text, :exam_id, :user_id)
+    end
 end
