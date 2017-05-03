2ffd224170639084f8f13c8ca02c43fec9c0c6ea
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-05-03 13:44:26 -0300

    fixing include on find

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -48,7 +48,7 @@ class ExamsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_exam
-      @exam = Exam.find(params[:id]).includes(:subject)
+      @exam = Exam.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
