694358abe8f7967c8d4f06b9c7d1cdda8501e26d
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-06-10 13:00:37 -0300

    changing from patch to post

--- config/application.rb
+++ config/application.rb
@@ -41,8 +41,8 @@ module Truckvilleapi
       config.fog_provider = 'fog/aws'                        # required
       config.fog_credentials = {
         provider:              'AWS',                        # required
-        aws_access_key_id:     ENV["S3_ACCESS_KEY"],                        # required
-        aws_secret_access_key: ENV["S3_SECRET_ACCESS"],                        # required
+        aws_access_key_id:     '',                        # required
+        aws_secret_access_key: '',                        # required
         region:                'us-west-2',                  # optional, defaults to 'us-east-1'
         endpoint: 'http://s3-us-west-2.amazonaws.com',
       }
