304bd58b57e10c5b7f6b1a5351c5dd36ac9bad0d
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-07-05 18:22:42 -0300

    adding sort to subjects

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -1,6 +1,6 @@
 class Api::CoursesController < ApplicationController
   def subjects_in_courses
-    @subjects = Course.find(params[:id]).subjects.order(:name)
+    @subjects = Course.find(params[:id]).subjects.order(name: :asc)
     render json: @subjects.to_json(), status: :ok
   end
 end
