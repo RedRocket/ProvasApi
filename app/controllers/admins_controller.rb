bc3269b23920a8dd3f507c707c29ea4593fbbb9d
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-07-18 14:07:48 -0300

    fixing admin delete account

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,5 @@
 class AdminsController < ApplicationController
+
   def edit
     @admin = current_admin
   end
@@ -32,6 +33,16 @@ class AdminsController < ApplicationController
       else
         format.html { redirect_to :back, notice: 'Falha ao criar' }
       end
+    end
+  end
+
+  def destroy
+    @admin = Admin.find(params[:id])
+
+    if @admin == current_admin
+      redirect_to root_path, notice: "Administrador apagado"
+    else
+      redirect_to admins_path, notice: "Administrador apagado"
     end
   end
 
