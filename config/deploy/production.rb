5f6f36bb561b32c2d55bb24e3fb972f18e8d74a3
Guilherme Carneiro <gui.carneiro15@gmail.com>
2017-08-08 16:36:59 -0300

    changing dns

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '52.33.227.78', user: 'deploy', roles: %w{web app db}
+server '34.213.176.166', user: 'deploy', roles: %w{web app db}
 
 # role-based syntax
 # ==================
